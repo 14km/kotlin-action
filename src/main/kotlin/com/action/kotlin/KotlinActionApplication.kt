1bb7ad98bfecf737440867040f2e73383fc5b187
Connor Kim <k.connor614@gmail.com>
2021-08-31 00:10:18 +0900

    project push

---
+++ src/main/kotlin/com/action/kotlin/KotlinActionApplication.kt
@@ -0,0 +1,11 @@
+package com.action.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinActionApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinActionApplication>(*args)
+}
---
+++ src/test/kotlin/com/action/kotlin/KotlinActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.action.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinActionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
